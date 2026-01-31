297b9b0a423d67dc19f465e76bc7151e63778048
Eak Netpanya <eak.netpanya@gmail.com>
2026-02-01 04:25:36 +0700

    add one-to-many

---
+++ one-to-many/ecto_assoc/lib/ecto_assoc/post.ex
@@ -0,0 +1,9 @@
+defmodule EctoAssoc.Post do
+  use Ecto.Schema
+
+  schema "posts" do
+    field(:header, :string)
+    field(:body, :string)
+    belongs_to(:user, EctoAssoc.User)
+  end
+end
---
+++ one-to-many/ecto_assoc/lib/ecto_assoc/user.ex
@@ -0,0 +1,9 @@
+defmodule EctoAssoc.User do
+  use Ecto.Schema
+
+  schema "users" do
+    field(:name, :string)
+    field(:email, :string)
+    has_many(:posts, EctoAssoc.Post)
+  end
+end
