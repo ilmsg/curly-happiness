3d0ab4fbe2f3c8d46397362430afbab534670c08
Eak Netpanya <eak.netpanya@gmail.com>
2026-02-01 04:25:46 +0700

    add many-to-many

---
+++ many-to-many/ecto_assoc/lib/ecto_assoc/post.ex
@@ -0,0 +1,11 @@
+defmodule EctoAssoc.Post do
+  use Ecto.Schema
+
+  schema "posts" do
+    field(:header, :string)
+    field(:body, :string)
+
+    # many_to_many :tags, EctoAssoc.Tag, join_through: "posts_tags"
+    many_to_many :tags, EctoAssoc.Tag, join_through: "posts_tags", on_replace: :delete
+  end
+end
